cadf2fd853efec588143d6ea7cef7e0526ce2276
Kavindu Ariyasinghe <kavindumalshan@gmail.com>
2020-08-15 12:10:34 -0700

    Screen 1 Interface Updated

--- src/components/SideLinks.js
+++ src/components/SideLinks.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './SideLinks.css';
+import {Row} from 'react-bootstrap'
+import {Container} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
 
 export class SideLinks extends React.Component {
   render() {
     return (
-        <div className="sideLinks">
-          <header className="App-header">
-             <a href="https://www.w3schools.com" className="sideLink1">Upload your slides</a>
-             <a href="https://www.w3schools.com" className="sideLink2">User Guid</a>
-          </header>
-        </div>
+          <div className="sideLinks">
+            <header className="App-header">
+            <Container>
+              <Row>
+                <Col sm={6.5}>
+                  <span class="btn default btn-file">
+                      <span class="fileinput-new">
+                        Upload your slides
+                      </span>
+                      <input type="file" class="textbox wp95 fl fileupload fileInput" name="files" id="fileupload"/>
+                  </span>
+                </Col>
+                <Col sm={5.5}>
+                  <span class="btn default btn-file">
+                    <a href="https://www.w3schools.com">User Guid</a>
+                  </span>
+                </Col>
+              </Row>
+            </Container>
+            </header>
+          </div>
       );
   }
 }
